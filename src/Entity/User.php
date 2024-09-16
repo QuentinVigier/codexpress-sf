1e0f4b9fbc904c150688e96965fc70b41a5d8be3
Jensone <hello@jensone.com>
2024-09-16 15:29:55 +0200

    🔧 (composer.json): add symfonycasts/verify-email-bundle to dependencies 🔧 (config/bundles.php): register SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle ✨ (RegistrationController.php): introduce RegistrationController to handle user registration and email verification 📝 (User.php): add UniqueEntity constraint to ensure email uniqueness 📝 (User.php): add isVerified property and methods to handle email verification status 📝 (RegistrationFormType.php): create form type for user registration with email, password, and terms agreement fields 📝 (EmailVerifier.php): implement EmailVerifier class to handle email confirmation logic 📝 (confirmation_email.html.twig): create email confirmation template 📝 (register.html.twig): create registration form template

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -13,6 +14,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Table(name: '`user`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 #[ORM\HasLifecycleCallbacks]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -49,6 +51,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column]
     private ?\DateTimeImmutable $updated_at = null;
+
+    #[ORM\Column]
+    private bool $isVerified = false;
 
     public function __construct()
     {
@@ -206,6 +211,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setUpdatedAt(\DateTimeImmutable $updated_at): static
     {
         $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
